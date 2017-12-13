b49202309003fb9a0207921581a9e3ddbcc726a1
root <root@DESKTOP-8I67NLH.localdomain>
2017-12-13 05:18:50 +0000

    Adição da Porta

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const formidable = require('express-formidable');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function(req, res){
    res.sendFile(path.join(__dirname + '/index.html'));
 
@@ -25,5 +27,6 @@ app.post('/', function(req, res){
     res.status(200).send(req.fields);
 
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
