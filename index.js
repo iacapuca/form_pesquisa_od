7fec68b995c3d727df706160312db43e7c2d0a7f
root <root@DESKTOP-8I67NLH.localdomain>
2017-12-13 23:10:26 +0000

    Adição de timestamp ao nome do arquivo

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const formidable = require('express-formidable');
+var formidable = require('express-formidable');
+var time = require('express-timestamp')
 var app = express();
 var path = require('path');
 
 app.set('port', (process.env.PORT || 3000));
+app.use(time.init);
 
-app.get('/', function(req, res){
-   res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 
 });
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/images", express.static(__dirname + '/images'));
 app.use("/styles", express.static(__dirname + '/styles'));
+
 
 app.use(formidable());
 
@@ -20,11 +23,11 @@ app.use(formidable());
 // for parsing multipart/form-data
 app.use(express.static('public'));
 
-app.post('/', function(req, res){
-   console.log(req.fields);
-   res.header("Content-Disposition", "attachment;filename="+"teste"+".csv");
-    res.type("text/csv");
-    res.status(200).send(req.fields);
+app.post('/', function(req, res) {
+  console.log(Object.values(req.fields));
+  res.header("Content-Disposition", "attachment;filename=" + req.timestamp.tz("America/Sao_Paulo").format() + ".csv");
+  res.type("text/csv");
+  res.status(200).send(Object.values(req.fields));
 
 });
 app.listen(app.get('port'), function() {
