de489100904fb22c00a38007ce251c7aba74091e
root <root@DESKTOP-8I67NLH.localdomain>
2018-03-12 11:49:49 -0300

    Modificações no mailer

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 const path = require('path');
 const nodemailer = require('nodemailer');
 const converter = require('json-2-csv');
+const os = require('os');
+const fs = require('fs');
+
 
 
 app.set('port', (process.env.PORT || 3000));
@@ -23,22 +26,65 @@ app.use("/styles", express.static(__dirname + '/styles'));
 
 app.use(formidable());
 
-
 // for parsing multipart/form-data
 app.use(express.static('public'));
 
 app.post('/', function(req, res) {
   let data = req.fields;
-  var json2csvCallback = function (err, csv) {
+  var json2csvCallback = function(err, csv) {
     if (err) throw err;
-    console.log(csv);
-};
 
-converter.json2csv(data, json2csvCallback);
-  console.log(data);
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Mensagem Enviada: %s', info.messageId);
+      // Preview only available when sending through an Ethereal account
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+
+  };
+
+  const output = `
+  <p><b>Hostname:</b>${os.hostname()}</p>
+  <p><b>Filename:</b>${req.timestamp.tz("America/Sao_Paulo").format()}</p>
+  <p>${Object.values(req.fields)}</p>`;
+
+  const csvbuilder = `${Object.values(req.fields)}`;
+
+  converter.json2csv(data, json2csvCallback);
   res.header("Content-Disposition", "attachment;filename=" + req.timestamp.tz("America/Sao_Paulo").format() + ".csv");
   res.type("text/csv");
   res.status(200).send(Object.values(req.fields));
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: 'ameciclo@gmail.com',
+      clientId: '620335039624-9rklbrm5g2gs2gqe77bhd9vc68hkkbbn.apps.googleusercontent.com',
+      clientSecret: 'UpAeX0YZKvjVzk4EjWysFYqw',
+      refreshToken: '1/bIF23qeYfEGokBt5Ln8KO0JJhZdGwWZiOnwrDy81JGk',
+      accessToken: 'ya29.Glt7Bbxv24fsksAdDckdEGthtnK1d7tbekKQCgK-PXumhXfqrOqes_v3uc1DUrHD5M1gCV3mGKjDj6v6qRLg-BYtjD4iTZasBUNK-vuqCDUHUJ2yuQI5MiqlV65S',
+    },
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Pesquisa OD da Ameciclo" <ameciclo@gmail.com>', // sender address
+    to: 'iacapuca@gmail.com, dvalenca@gmail.com', // list of receivers
+    subject: 'Nova resposta a pesquisa OD!', // Subject line
+    html: output,
+    attachments: [{
+      filename: 'Resultado da Pesquisa OD.csv',
+      content: csvbuilder
+    }]
+  };
+
 
 });
 app.listen(app.get('port'), function() {
